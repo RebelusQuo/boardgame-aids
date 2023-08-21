e629359b7391fce932a492184906a55c0be9517e
RebelusQuo <rebelusquo@tutanota.com>
2023-08-21 15:11:43 +0200

    Implement a persistent aid selection
    The custom hook `useLocalStorage` handles local storage variables.
    
    The local storage key "selected-aid" holds the object key for the currently
    shown aid. A temporary placeholder aid is used in this commit to verify the
    behavior.

--- js/App.jsx
+++ js/App.jsx
@@ -1,3 +1,26 @@
+import { Suspense, lazy } from 'react';
+import { useLocalStorage } from './useLocalStorage';
+
+import map from 'lodash/map';
+import find from 'lodash/find';
+import kebabCase from 'lodash/kebabCase'
+
+const aids = [
+    { name: 'Placeholder', Component: lazy(() => import('./aids/placeholder')) }
+];
+
 export function App() {
-    return null;
+    const [selectedAid, setSelectedAid] = useLocalStorage('selected-aid', '');
+    const aid = find(aids, { name: selectedAid });
+
+    return <>
+        <select value={selectedAid} onChange={e => setSelectedAid(e.target.value)}>
+            {selectedAid ? null : <option/>}
+            {map(aids, ({ name }) => <option key={kebabCase(name)} value={name}>{name}</option> )}
+        </select>
+        <hr/>
+        <Suspense fallback={<div>•••</div>}>
+            {aid && <aid.Component/>}
+        </Suspense>
+    </>;
 };
